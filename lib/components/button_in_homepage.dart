0dd1acb6d3202e320cfda8392ee34082dd97af35
AhmedHamouda <ahmedhamooda221@gmail.com>
2024-11-15 19:41:51 +0200

    refactor and add functionality for push nextpage

--- lib/components/button_in_homepage.dart
+++ lib/components/button_in_homepage.dart
@@ -4,22 +4,37 @@ class ButtonInHomepage extends StatelessWidget
 {
   Color color;
   String title;
-  ButtonInHomepage({required this.color,required this.title});
+  Widget nextPage;
+  //Function() ontap;
+  ButtonInHomepage({required this.color,required this.title,required this.nextPage});
   @override
   Widget build(BuildContext context) 
   {
     return GestureDetector
     (
-     onTap: (){print("$title page");}, 
+      onTap:()
+      {
+        Navigator.push
+        (
+          context, 
+          MaterialPageRoute
+          (
+            builder: (context)
+            {
+              return nextPage;
+            }
+          )
+        );
+      },
       child: Container
       (
         //alignment:Alignment.centerLeft,
         color: color ,
         height: 75,
         width: double.infinity,
-        margin: EdgeInsets.all(15),
+        margin: const EdgeInsets.all(15),
         //padding: EdgeInsets.all(15),
-        child: Center(child: Text("$title",style:TextStyle(fontSize: 25,color: Colors.white),)),
+        child: Center(child: Text(title,style: const TextStyle(fontSize: 25,color: Colors.white),)),
       ),
     );
   }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:language_learning_app/components/button_in_homepage.dart';
+import 'package:language_learning_app/screens/number.dart';
 
 class HomePage extends StatelessWidget 
 {
@@ -17,16 +18,37 @@ class HomePage extends StatelessWidget
           "Language Learning App",
           style: TextStyle(color: const Color.fromARGB(255, 223, 213, 213)),
         ),
-        backgroundColor:const  Color(0xff3E2E27),
+        backgroundColor:const  Color(0xff3E2E27), 
       ),
       body: Column
       (
         children: 
         [
-          ButtonInHomepage(color: const Color(0xffED8F31), title: "Numbers"),
-          ButtonInHomepage(color: const Color(0xff528032), title: "Family Member"),
-          ButtonInHomepage(color: const Color(0xff7D40A2), title: "Colors"),
-          ButtonInHomepage(color: const  Color(0xff47A5CB), title: "Phrases"),
+          ButtonInHomepage
+          (
+            color: const Color(0xffED8F31),
+            title: "Numbers",
+            nextPage: NumbersPage(),
+           
+          ),
+          ButtonInHomepage
+          (
+            color: const Color(0xff528032),
+            title: "Family Member",
+            nextPage: NumbersPage(),
+          ),
+          ButtonInHomepage
+          (
+            color: const Color(0xff7D40A2),
+            title: "Colors",
+            nextPage: NumbersPage(),
+          ),
+          ButtonInHomepage
+          (
+            color: const Color(0xff47A5CB),
+            title: "Phrases",
+            nextPage: NumbersPage(),
+          ),
         ],
       ),
     );
